6b071e48e73b0e495e42cb17d16e7737bfea0e0b
Sean <xuelun.lu@gmail.com>
2020-02-10 07:33:10 +0800

    no message

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,4 +1,8 @@
-<?php if (!defined('BASEPATH')) {
+<?php
+
+use \Illuminate\Database\Capsule\Manager as Capsule;
+
+if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
@@ -9,6 +13,29 @@ class MY_Controller extends CI_Controller
     {
 
         parent::__construct();
+
+        require_once dirname(dirname(dirname(__FILE__))) . '/vendor/autoload.php';
+        require_once dirname(dirname(__FILE__)) . '/config/database.php';
+
+        $capsule = new Capsule();
+
+        $capsule->addConnection([
+            'driver'    => 'mysql',
+            'host'      => $db['default']['hostname'],
+            'port'      => $db['default']['port'],
+            'username'  => $db['default']['username'],
+            'password'  => $db['default']['password'],
+            'database'  => $db['default']['database'],
+            'charset'   => $db['default']['char_set'],
+            'collation' => $db['default']['dbcollat'],
+            'prefix'    => '',
+            'options'   => array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+            ),
+        ], 'website');
+
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
 
     }
 
